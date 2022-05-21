d2f5221a61cbfea8cb1bb38fef031edd1535bae9
Oluwatomisin Taiwo <oluwatomisinoluwafemi@gmail.com>
2022-05-21 19:12:05 +0100

    app modules for routing and lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
+import { LandingModule } from './components/landing/landing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +16,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    AppRoutingModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      preventDuplicates: true
+    }),
+    SharedModule,
+    LandingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
